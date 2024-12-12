b7f6249e80be3994a1dfa295d6cf22aa8df29cb7
thefool309 <themagician309@gmail.com>
2024-12-12 10:50:23 -0600

    Made updates to the main.rs and created the rust book example guessing game!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
+use rand::Rng;
+
 fn main() { //comments work like c or c++
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..100);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("The Secret Number is : {}", secret_number);
+
+    println!("You guessed: {}", guess);
+
+
+
+
 }
